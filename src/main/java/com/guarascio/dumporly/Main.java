a79e7ea2e94763d6a2584c1d9a378ecab00b7c95
guarale@hotmail.com <guarale@hotmail.com>
2020-05-02 19:29:56 +0200

    Cycling on "Clean code" videos
    It doesn't still work
    TODO Needs authentication

--- src/main/java/com/guarascio/dumporly/Main.java
+++ src/main/java/com/guarascio/dumporly/Main.java
@@ -1,7 +1,9 @@
 package com.guarascio.dumporly;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
@@ -9,13 +11,19 @@ public class Main {
         WebDriverManager.chromedriver().version("81").setup();
         WebDriver driver = new ChromeDriver();
 
-        String baseUrl = "http://www.repubblica.it";
+        String baseUrl = "https://learning.oreilly.com/videos/clean-code/9780134661742/9780134661742-CODE_01_00_00";
 
-        // launch Fire fox and direct it to the Base URL
         driver.get(baseUrl);
 
-        Thread.sleep(5000);
+        // TODO Perform authentication
 
-        driver.close();
+        while(true) {
+            System.out.println(driver.getCurrentUrl());
+            Thread.sleep(5000);
+            WebElement element = driver.findElement(By.className("ContentNavigation-title-101_S"));
+            element.click();
+        }
+
+        //driver.close();
     }
 }
