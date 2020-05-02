fb2f283b1ad512d4af420dac766c116e5ed37c70
guarale@hotmail.com <guarale@hotmail.com>
2020-05-02 22:26:12 +0200

    Authentication implemented

--- src/main/java/com/guarascio/dumporly/Authenticator.java
+++ src/main/java/com/guarascio/dumporly/Authenticator.java
@@ -3,6 +3,7 @@ package com.guarascio.dumporly;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Authenticator {
     private final WebDriver driver;
@@ -11,9 +12,22 @@ public class Authenticator {
         this.driver = driver;
     }
 
-    public void authenticate() {
-
+    public void authenticate(String userName, String password) {
         WebElement element = driver.findElement(By.className("t-sign-in"));
         element.click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(d -> d.findElement(By.name("email")));
+
+        WebElement mail = driver.findElement(By.name(
+                "email"));
+        mail.sendKeys(userName);
+
+        WebElement pwd = driver.findElement(By.name(
+                "password"));
+        pwd.sendKeys(password);
+
+        WebElement signIn = driver.findElement(By.xpath(
+                "//button[text()='Sign In']"));
+        signIn.click();
     }
 }
--- src/main/java/com/guarascio/dumporly/Main.java
+++ src/main/java/com/guarascio/dumporly/Main.java
@@ -6,15 +6,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        String userName = "";
+        String password = "";
+        String baseUrl = "https://learning.oreilly.com/videos/clean-code/9780134661742/9780134661742-CODE_01_00_00";
+
         WebDriverManager.chromedriver().version("81").setup();
         WebDriver driver = new ChromeDriver();
-
-        String baseUrl = "https://learning.oreilly.com/videos/clean-code/9780134661742/9780134661742-CODE_01_00_00";
 
         driver.get(baseUrl);
 
         // TODO Perform authentication
-        new Authenticator(driver).authenticate();
+        new Authenticator(driver).authenticate(userName, password);
 /*
         while(true) {
             System.out.println(driver.getCurrentUrl());
@@ -23,6 +25,7 @@ public class Main {
             element.click();
         }
 */
-        //driver.close();
+        Thread.sleep(5000);
+        driver.close();
     }
 }
