18a1c8d6a8201e2ae42df38879260e603e6c01ad
guarale@hotmail.com <guarale@hotmail.com>
2020-05-02 19:17:48 +0200

    Refactor

--- src/main/java/com/guarascio/dumporly/Main.java
+++ src/main/java/com/guarascio/dumporly/Main.java
@@ -3,7 +3,6 @@ package com.guarascio.dumporly;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
