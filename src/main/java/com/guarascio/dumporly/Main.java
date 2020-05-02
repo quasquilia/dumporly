2bb2eedc1a4c61317aa61463b8010da0764ba842
guarale@hotmail.com <guarale@hotmail.com>
2020-05-02 19:17:19 +0200

    Opening navigating and closing Chrome

--- src/main/java/com/guarascio/dumporly/Main.java
+++ src/main/java/com/guarascio/dumporly/Main.java
@@ -1,7 +1,22 @@
 package com.guarascio.dumporly;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(args[0]);
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().version("81").setup();
+        WebDriver driver = new ChromeDriver();
+
+        String baseUrl = "http://www.repubblica.it";
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        Thread.sleep(5000);
+
+        driver.close();
     }
 }
